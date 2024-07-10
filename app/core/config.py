a44f020ee5629bea50fb70b64cec40aa7f853ba4
Natalija <vukosavljenatalija@gmail.com>
2024-07-10 21:45:40 +0200

    changed config

--- app/core/config.py
+++ app/core/config.py
@@ -2,10 +2,6 @@
 
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 JWT_REFRESH_SECRET_KEY = os.getenv("JWT_REFRESH_SECRET_KEY")
